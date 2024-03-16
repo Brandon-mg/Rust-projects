d9e251bd34c153507d1b9380354912fed24db64c
Numa <reachbmg@gmail.com>
2024-03-16 00:22:18 -0700

    new file:   Types_conversions/conversions.exe 	new file:   Types_conversions/conversions.pdb 	new file:   Types_conversions/conversions.rs 	new file:   flow_control/flows.exe 	new file:   flow_control/flows.pdb 	new file:   flow_control/flows.rs

---
+++ flow_control/flows.rs
@@ -0,0 +1,166 @@
+#![allow(unreachable_code, unused_labels)]
+
+fn main() {
+    let n = 5;
+
+    if n < 0 {
+        print!("{} is negative", n);
+    } else if n > 0 {
+        print!("{} is positive", n);
+    } else {
+        print!("{} is zero", n);
+    }
+
+    let big_n =
+        if n < 10 && n > -10 {
+            println!(", and is a small number, increase ten-fold");
+
+            // This expression returns an `i32`.
+            10 * n
+        } else {
+            println!(", and is a big number, halve the number");
+
+            // This expression must return an `i32` as well.
+            n / 2
+            // TODO ^ Try suppressing this expression with a semicolon.
+        };
+    //   ^ Don't forget to put a semicolon here! All `let` bindings need it.
+
+    println!("{} -> {}", n, big_n);
+
+    let mut count = 0u32;
+
+    'looplabelouter: loop {
+        println!("{}", count);
+        count += 1;
+        if count == 5 {
+            'looplabelinner: loop{
+                count+=1;
+                println!("innerloop {}", count);
+                break 'looplabelouter;
+            }
+            println!("unreachable");
+            break;
+        }
+    }
+
+    let result = loop {
+        count += 1;
+
+        if count == 10 {
+            break count * 2;
+        }
+    };
+    println!("{}", result);
+    assert_eq!(result, 20);
+
+    while count < 100 {
+        if count % 15 == 0{
+            println!("fizzbuzz");
+        } else if count % 3 == 0{
+            println!("fizz");
+        } else if count % 5 == 0{
+            println!("buzz");
+        } else {
+            println!("{}", count)
+        }
+        count += 1;
+    }
+
+    for n in 1..=100 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    let names1 = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names1.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            // TODO ^ Try deleting the & and matching just "Ferris"
+            _ => println!("Hello {}", name),
+        }
+    }
+    
+    println!("names: {:?}", names1);
+
+    let names2 = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names2.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+
+    let mut names3 = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names3.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+
+    println!("names: {:?}", names3);
+
+
+    let triple = (0, -2, 3);
+    // TODO ^ Try different values for `triple`
+
+    println!("Tell me about {:?}", triple);
+    // Match can be used to destructure a tuple
+    match triple {
+        // Destructure the second and third elements
+        (0, y, z) => println!("First is `0`, `y` is {:?}, and `z` is {:?}", y, z),
+        (1, ..)  => println!("First is `1` and the rest doesn't matter"),
+        (.., 2)  => println!("last is `2` and the rest doesn't matter"),
+        (3, .., 4)  => println!("First is `3`, last is `4`, and the rest doesn't matter"),
+        // `..` can be used to ignore the rest of the tuple
+        _      => println!("It doesn't matter what they are"),
+        // `_` means don't bind the value to a variable
+    }
+
+    let array = [1, -2, 6];
+
+    match array {
+        // Binds the second and the third elements to the respective variables
+        [0, second, third] =>
+            println!("array[0] = 0, array[1] = {}, array[2] = {}", second, third),
+
+        // Single values can be ignored with _
+        [1, _, third] => println!(
+            "array[0] = 1, array[2] = {} and array[1] was ignored",
+            third
+        ),
+
+        // You can also bind some and ignore the rest
+        [-1, second, ..] => println!(
+            "array[0] = -1, array[1] = {} and all the other ones were ignored",
+            second
+        ),
+        // The code below would not compile
+        // [-1, second] => ...
+
+        // Or store them in another array/slice (the type depends on
+        // that of the value that is being matched against)
+        [3, second, tail @ ..] => println!(
+            "array[0] = 3, array[1] = {} and the other elements were {:?}",
+            second, tail
+        ),
+
+        // Combining these patterns, we can, for example, bind the first and
+        // last values, and store the rest of them in a single array
+        [first, middle @ .., last] => println!(
+            "array[0] = {}, middle = {:?}, array[2] = {}",
+            first, middle, last
+        ),
+    }
+}
